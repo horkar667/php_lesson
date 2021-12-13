6071490389003b300ea185426b8aad5341748fc0
horkar667 <jmmpp413@yahoo.co.jp>
2021-12-13 20:40:48 +0900

    [Update]Lesson3

--- Lesson3/index.php
+++ Lesson3/index.php
@@ -14,18 +14,22 @@
   <body>
     <div class="menu-wrapper container">
       <h1 class="logo">Cafe</h1>
-      <h3>メニュー<?php echo Menu::getCount() ?></h3>
+      <h3>メニュー<?php echo Menu::getCount() ?>品</h3>
       <form method="post" action="confirm.php">
         <div class="menu-items">
           <?php foreach($menus as $menu): ?>
             <div class="menu-item">
               <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
-              <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
+              <h3 class="menu-item-name">
+                <a href="show.php?name=<?php echo $menu->getName() ?>">
+                  <?php echo $menu->getName() ?>
+                </a>
+              </h3>
               <?php if($menu instanceof Drink): ?>
                 <p class="menu-item-name"><?php echo $menu->getType() ?></p>
               <?php else: ?>
                 <?php for($i = 0; $i < $menu->getSpiciness(); $i++): ?>
-                  <img src="https://s3-ap-northeast-1.amazonaws.com/prigate/shared/images/lesson/php/chilli.png" class="icon-spiciness">
+                  <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/chilli.png" class="icon-spiciness">
                 <?php endfor ?>
               <?php endif ?>
               <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?>（税込）</p>
